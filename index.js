4ae1c66769e6fb5e96148ff7c35afa57b98d75e7
gabriell <gabriel@smit.net.br>
2021-03-11 21:33:06 -0300

    Primeira versão

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+ 
+var apiPort = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.get('/test', function (req, res) {
+    res.send('Hello World Teste')
+  })
+  
+app.listen(apiPort)
