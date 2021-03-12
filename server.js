10c2926f5cb3bf054aa2ec354e49f4b89f5b5bc3
gabriell <gabriel@smit.net.br>
2021-03-11 23:41:25 -0300

    Mais um teste

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.get('/test', function (req, res) {
+app.get('/test/', function (req, res) {
     res.send('Hello World Teste')
   })
   
